8a296fa9ef1f95a2659ca7d98cef158e57e83cdb
Latorreander <latorreander@gmail.com>
2023-04-20 16:01:29 -0300

    'resolução'

--- exercicio-11/app.js
+++ exercicio-11/app.js
@@ -5,6 +5,8 @@
   - Ela deve ter um escopo global.
 */
 
+const myName = 'Anderson'
+
 /*
   02
 
@@ -17,7 +19,21 @@
   - Um erro com a mensagem "Uncaught ReferenceError: SUA_VARIÁVEL is not  
     defined" será exibido no console;
   - Você sabe por que isso aconteceu?
+
 */
+
+
+const showAge = (value = 0) => {
+  let age = value
+ 
+  console.log(`${myName} tem ${age} anos de idade`)
+}
+
+showAge(37)
+
+// console.log(age)
+
+// R. A variável age não pode ser acessada fora do escopo que ela foi criada, ela está no escopo local da função 'showoAge', portanto ela não existe fora do escopo local.
 
 /*
   03
@@ -37,23 +53,55 @@
       "O NOME_DO_CARRO está disponível nas cores COR_01, COR_02 e COR_03".
 */
 
+const car = {
+  name: 'Corolla',
+  brand: 'Toyota',
+  colors: ['Preto', 'Branco', 'Prata'],
+  isRunning: false,
+  run: function (nameOfCar) {
+    nameOfCar = this.name
+    this.isRunning = true
+    if(this.isRunning === true){
+      console.log(`O ${nameOfCar} está em movimento.`)
+    }
+  },
+  stop: function (nameOfCar) {
+    nameOfCar = this.name
+    this.isRunning = false
+    if(this.isRunning === false) {
+      console.log(`O ${nameOfCar} está parado.`)
+    } 
+  },
+  getColors: function (nameOfCar, color) {
+    nameOfCar = this.name
+    color = this.colors
+    
+    console.log(`O ${nameOfCar} está disponível nas cores ${color[0]}, ${color[1]} e ${color[2]}.`)
+  }
+}
+
 /*
   04
 
   - Faça o carro andar e exiba no console se ele realmente está em movimento.
 */
+car.run()
+console.log(car.isRunning)
 
 /*
   05
 
   - Faça o carro parar e exiba no console se ele realmente está parado.
 */
-
+car.stop()
+console.log(car.isRunning)
 /*
   06
 
   - Exiba, no console, a mensagem com as cores do carro.
 */
+
+car.getColors()
 
 /*
   07
@@ -61,3 +109,5 @@
   - Exiba, no console, a mensagem "O carro é um MARCA_DO_CARRO NOME_DO_CARRO";
   - Utilize a notação de colchetes para acessar as propriedades do carro.
 */
+
+console.log(`O carro é um ${car['brand']} ${car['name']}`)
